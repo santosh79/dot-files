7e83921f274cc25b284a049563e4b097455fc180
Santosh Kumar <santosh79@gmail.com>
2011-10-15 12:29:39 -0700

    using scripts for starting/stopping multiple redis servers

--- start-redis-servers.rb
+++
@@ -1,5 +0,0 @@
-%W{redis.conf redis-second.conf redis-third.conf redis-fourth.conf
-   redis-fifth.conf redis-sixth.conf redis-seventh.conf redis-eight.conf
-   redis-ninth.conf}.each do |conf_file|
-    system("redis-server #{conf_file}")
-   end
--- stop-redis-servers.rb
+++
@@ -1,3 +0,0 @@
-9.times do |i|
-  system("redis-cli -p #{6379 + i*1000} SHUTDOWN")
-end
